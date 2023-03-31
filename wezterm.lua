701dfa13b90a7107a365bf38e5f38d9f6edf120e
Thorsten Rhau <thorsten@rhau.se>
2023-03-31 18:11:40 +0200

    Fish shell is now launched as part of the configuration

--- wezterm.lua
+++ wezterm.lua
@@ -5,14 +5,15 @@ local function use_dark()
 end
 
 return {
+  default_prog = { "/opt/homebrew/bin/fish", "-l" },
   font = wezterm.font_with_fallback({ "MonoLisa Fancy", "Symbols Nerd Font Mono" }),
   font_size = 13.0,
   color_scheme_dirs = { "~/.config/wezterm/colors" },
   -- First theme after the function is the dark theme
   color_scheme = use_dark() and "melange_dark" or "melange_light",
-  freetype_load_target = "Light", -- Light is good for macOS, default is 'Normal'
+  freetype_load_target = "Light",
   adjust_window_size_when_changing_font_size = false,
   hide_tab_bar_if_only_one_tab = true,
-  use_fancy_tab_bar = false,
+  tab_bar_at_bottom = true,
   audible_bell = "Disabled",
 }
