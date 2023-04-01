c52e1c43c2b8a473c8451cd4ee1d1ae7f0a88d3b
Thorsten Rhau <thorsten@rhau.se>
2023-04-01 17:16:49 +0200

    New theme and theme switcher for 'zenbones'

--- wezterm.lua
+++ wezterm.lua
@@ -1,20 +1,22 @@
 local wezterm = require("wezterm")
 
-local function use_dark()
-  return wezterm.gui.get_appearance():match("Dark.*")
+-- https://wezfurlong.org/wezterm/colorschemes/index.html
+local function scheme_for_appearance(appearance)
+  if appearance:find("Dark") then
+    return "zenbones_dark"
+  else
+    return "zenbones"
+  end
 end
 
 return {
   font = wezterm.font_with_fallback({ "MonoLisa Fancy", "Symbols Nerd Font Mono" }),
   font_size = 13.0,
   color_scheme_dirs = { "~/.config/wezterm/colors" },
-  -- First theme after the function is the dark theme
-  color_scheme = use_dark() and "nightfox" or "dayfox",
+  color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
   freetype_load_target = "Light",
-  freetype_render_target = "HorizontalLcd",
   adjust_window_size_when_changing_font_size = false,
   hide_tab_bar_if_only_one_tab = true,
   tab_bar_at_bottom = true,
   audible_bell = "Disabled",
-  term = "wezterm",
 }
