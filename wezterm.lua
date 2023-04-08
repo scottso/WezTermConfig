af1642bab8b596f712fedf2fb9113f420823b499
Thorsten Rhau <thorsten@rhau.se>
2023-04-08 14:09:06 +0200

    Moved 'freetype_load_target' before font config

--- wezterm.lua
+++ wezterm.lua
@@ -10,11 +10,11 @@ local function scheme_for_appearance(appearance)
 end
 
 return {
+  freetype_load_target = "Light",
   font = wezterm.font_with_fallback({ "MonoLisa Fancy", "Symbols Nerd Font Mono" }),
   font_size = 13.0,
   color_scheme_dirs = { "~/.config/wezterm/colors" },
   color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
-  freetype_load_target = "Light",
   adjust_window_size_when_changing_font_size = false,
   hide_tab_bar_if_only_one_tab = true,
   tab_bar_at_bottom = true,
