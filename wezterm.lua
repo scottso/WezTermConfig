1c2e79b38413a2c381d0e45a3e0af03e9e9dbf69
Thorsten Rhau <thorsten@rhau.se>
2023-04-10 10:45:00 +0200

    Added 'front_end' and tab configuration

--- wezterm.lua
+++ wezterm.lua
@@ -12,10 +12,14 @@ local function scheme_for_appearance(appearance)
 end
 
 return {
+  front_end = "WebGpu", -- Render with Metal on macOS
   freetype_load_target = "Light",
   color_scheme_dirs = { "~/.config/wezterm/colors" },
   color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
-  font = wezterm.font_with_fallback({ "MonoLisa Fancy", "Symbols Nerd Font Mono" }),
+  font = wezterm.font_with_fallback({
+    "MonoLisa Fancy",
+    "Symbols Nerd Font Mono",
+  }),
   font_size = 13.0,
   initial_rows = 40,
   initial_cols = 110,
@@ -24,4 +28,9 @@ return {
   hide_tab_bar_if_only_one_tab = true,
   tab_bar_at_bottom = true,
   audible_bell = "Disabled",
+  -- Tab bar configuration
+  window_frame = {
+    font = wezterm.font({ family = "MonoLisa Fancy", weight = "Medium" }),
+    font_size = 10.0,
+  },
 }
