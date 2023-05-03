0960f9c00b8b39256dc7d9ef3a538672284cf530
Thorsten Rhau <thorsten@rhau.se>
2023-05-03 18:29:45 +0200

    Back to zenbones

--- wezterm.lua
+++ wezterm.lua
@@ -4,10 +4,10 @@ local wezterm = require("wezterm")
 local function scheme_for_appearance(appearance)
   if appearance:find("Dark") then
     -- Dark theme
-    return "nightfox"
+    return "Zenbones_dark"
   else
     -- Light theme
-    return "dayfox"
+    return "Zenbones_light"
   end
 end
 
