53a3038c435f2590d2ba6e47876673dac7176670
Thorsten Rhau <thorsten@rhau.se>
2023-05-03 17:56:48 +0200

    Zenbones

--- wezterm.lua
+++ wezterm.lua
@@ -4,10 +4,10 @@ local wezterm = require("wezterm")
 local function scheme_for_appearance(appearance)
   if appearance:find("Dark") then
     -- Dark theme
-    return "Tokyo Night Storm"
+    return "Zenbones_dark"
   else
     -- Light theme
-    return "Tokyo Night Day"
+    return "Zenbones_light"
   end
 end
 
