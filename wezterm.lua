23b3a9663f25d352f9bac3c6bd35ec4c0d8e1989
Thorsten Rhau <thorsten@rhau.se>
2023-05-07 20:18:37 +0200

    Configure font stylistic sets

--- wezterm.lua
+++ wezterm.lua
@@ -16,9 +16,31 @@ return {
   freetype_load_target = "Light",
   color_scheme_dirs = { "~/.config/wezterm/colors" },
   color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+  harfbuzz_features = {
+    "calt=1", -- https://docs.microsoft.com/en-us/typography/opentype/spec/features_ae#tag-calt
+    "clig=1", -- https://docs.microsoft.com/en-us/typography/opentype/spec/features_ae#tag-clig
+    "liga=1", -- Ligatures
+    "zero=0", -- 0 with slash or dot
+    "ss01=0", -- * Asterisk normal or raised
+    "ss02=0", -- Script variant of font
+    "ss06=0", -- @ variants
+    "ss07=1", -- {} variations
+    "ss08=1", -- () variations
+    "ss09=1", -- >= 2 sharacters wide or not
+    "ss10=0", -- >= Alternate appearance for 1 char width
+    "ss11=1", -- 0xF Alternate hex appearance
+    "ss12=0", -- \\ Thin backslash (ruins ASCII art)
+  },
   font = wezterm.font_with_fallback({
-    { family = "MonoLisa Fancy" },
-    { family = "Symbols Nerd Font Mono", scale = 0.8 },
+    {
+      family = "MonoLisa",
+      weight = "Medium",
+    },
+    {
+      -- Fallback font with all the Netd Font Symbols
+      family = "Symbols Nerd Font Mono",
+      scale = 0.8,
+    },
   }),
   font_size = 13.0,
   initial_rows = 32,
