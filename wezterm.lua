1ef324a22f78d646868ef913e3b34bf8aacd1158
Thorsten Rhau <thorsten@rhau.se>
2023-12-03 15:29:38 +0100

    Changed to 'OneDark' and 'OneLight' themes

--- wezterm.lua
+++ wezterm.lua
@@ -5,10 +5,10 @@ local wezterm = require("wezterm")
 local function scheme_for_appearance(appearance)
   if appearance:find("Dark") then
     -- Returns OneDark (base16) theme for dark appearance
-    return "tokyonight_storm"
+    return "OneDark (base16)"
   else
     -- Returns One Light (base16) theme for light appearance
-    return "tokyonight_day"
+    return "One Light (base16)"
   end
 end
 
