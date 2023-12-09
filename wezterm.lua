df741ddecdc5d8c4c6f6b6c15532e9c872bfea87
Thorsten Rhau <thorsten@rhau.se>
2023-12-09 14:47:22 +0100

    Added new configuration based on learnings from Folke

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,8 @@
 -- Importing the wezterm module
 local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+require("links").setup(config)
 
 -- Function to set the color scheme based on the appearance (dark or light)
 local function scheme_for_appearance(appearance)
@@ -12,27 +15,17 @@ local function scheme_for_appearance(appearance)
   end
 end
 
-local config = {}
-
--- Conditional config builder
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
-
 -- Front-end renderer configuration
 config.front_end = "WebGpu" -- Use WebGpu for rendering
+config.webgpu_power_preference = "HighPerformance"
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
 config.freetype_load_target = "Light"
-config.color_scheme_dirs = { "~/.config/wezterm/colors" }
 
 -- Detect current appearance and apply the corresponding color scheme and tab style
+-- config.color_scheme_dirs = { "~/git/tokyonight.nvim/extras/wezterm" }
 local appearance = wezterm.gui.get_appearance()
 config.color_scheme = scheme_for_appearance(appearance)
-
--- Window frame configuration
-config.window_frame = {
-  font = wezterm.font({ family = "MonoLisa Variable", weight = "Black" }),
-  font_size = 10.0,
-}
 
 -- Font features configuration
 config.harfbuzz_features = {
@@ -54,6 +47,29 @@ config.font = wezterm.font_with_fallback({
   { family = "MonoLisa Variable", scale = 1.0, weight = "Regular" },
   { family = "Symbols Nerd Font Mono", scale = 0.9, weight = "Regular" },
 })
+-- Window frame configuration
+config.window_frame = {
+  font = wezterm.font({ family = "MonoLisa Variable", weight = "Black" }),
+  font_size = 10.0,
+}
+
+config.font_rules = {
+  {
+    intensity = "Bold",
+    italic = true,
+    font = wezterm.font({ family = "MonoLisa Variable", weight = "Bold", style = "Italic" }),
+  },
+  {
+    italic = true,
+    intensity = "Half",
+    font = wezterm.font({ family = "MonoLisa Variable", weight = "DemiBold", style = "Italic" }),
+  },
+  {
+    italic = true,
+    intensity = "Normal",
+    font = wezterm.font({ family = "MonoLisa Variable", style = "Italic" }),
+  },
+}
 
 -- General configuration settings
 config.font_size = 14.0
@@ -66,9 +82,16 @@ config.tab_bar_at_bottom = false
 config.audible_bell = "Disabled"
 config.window_close_confirmation = "AlwaysPrompt"
 config.window_background_opacity = 1 -- Full opacity
-config.window_decorations = "RESIZE|TITLE"
+config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = true
 config.term = "wezterm"
+config.underline_thickness = 3
+config.cursor_thickness = 4
+config.underline_position = -6
+config.bold_brightens_ansi_colors = true
+config.default_cursor_style = "BlinkingBar"
+config.force_reverse_video_cursor = true
+config.window_padding = { left = 5, right = 5, top = 5, bottom = 5 }
 
 -- Moving tabs
 config.keys = {}
