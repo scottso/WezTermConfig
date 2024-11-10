fec70aba88ea931fa2621aff8d0df2cf10efddf8
Scott Lampert <scott@lampert.org>
2024-11-09 20:58:16 -0500

    Add tabline plugin

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,10 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+tabline.setup()
+tabline.apply_to_config(config)
+
 require("links").setup(config)
 
 config.font = wezterm.font_with_fallback({
@@ -57,7 +61,7 @@ config.font = wezterm.font_with_fallback({
             "cv17",
         },
     },
-    { family = "Noto Color Emoji", weight = "Regular" },
+    { family = "Noto Color Emoji",  weight = "Regular" },
     { family = "Symbols Nerd Font", weight = "Regular" },
 })
 
