4bb906466ccb59b56e77a6596f4a0c68cd24bed4
Scott Lampert <slampert@salesforce.com>
2025-09-10 10:23:47 -0400

    Fix openurls

--- startup.lua
+++ startup.lua
@@ -6,13 +6,13 @@ wezterm.on("gui-startup", function(cmd)
     window:gui_window():toggle_fullscreen()
 end)
 
-wezterm.on("open-uri", function(window, pane, uri)
-    local mods, _ = window:keyboard_modifiers()
-    if string.find(mods, "SUPER") then
-        return true
-    else
-        window:copy_to_clipboard(uri)
-    end
-    -- prevent the default action from opening in a browser
-    return false
-end)
+-- wezterm.on("open-uri", function(window, pane, uri)
+--     local mods, _ = window:keyboard_modifiers()
+--     if string.find(mods, "SUPER") then
+--         return true
+--     else
+--         window:copy_to_clipboard(uri)
+--     end
+--     -- prevent the default action from opening in a browser
+--     return false
+-- end)
